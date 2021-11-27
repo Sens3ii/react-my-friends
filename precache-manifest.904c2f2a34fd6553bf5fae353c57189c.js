f17a65c83b3b452d49082ae8feb6365249017b13
Sens3ii <densteeper@gmail.com>
2021-11-27 13:34:04 +0600

    Updates

---
+++ precache-manifest.904c2f2a34fd6553bf5fae353c57189c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed1edbeff8c4a64c18e6",
+    "url": "/react-my-friends/static/css/main.541609c9.chunk.css"
+  },
+  {
+    "revision": "ed1edbeff8c4a64c18e6",
+    "url": "/react-my-friends/static/js/main.ed1edbef.chunk.js"
+  },
+  {
+    "revision": "559e2b864567de0b1875",
+    "url": "/react-my-friends/static/css/1.39a9cdf6.chunk.css"
+  },
+  {
+    "revision": "559e2b864567de0b1875",
+    "url": "/react-my-friends/static/js/1.559e2b86.chunk.js"
+  },
+  {
+    "revision": "cbe0871fd88b83d8dd36",
+    "url": "/react-my-friends/static/js/runtime~main.cbe0871f.js"
+  },
+  {
+    "revision": "cb00071ab72324a59623446c26244731",
+    "url": "/react-my-friends/static/media/background.cb00071a.jpg"
+  },
+  {
+    "revision": "3f59a769fd2ccc2ad41fa1fe2dc659ca",
+    "url": "/react-my-friends/index.html"
+  }
+];
